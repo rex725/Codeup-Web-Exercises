bde1b5d1061e162c940f7ef5e785f17e46a966ef
David R. Jex <david.jex@me.com>
2017-06-29 11:17:54 -0500

    jquery Attributes and Methods exercises.

---
+++ public/js/jquery_faq.js
@@ -0,0 +1,35 @@
+$('document').ready(function() {
+	$('a').click(function() {
+		$('dd').toggleClass('invisible');
+	});
+	$('#fact_1').click(function() {
+		$('#fact_1').toggleClass('highlight');
+	});
+	$('#fact_2').click(function() {
+		$('#fact_2').toggleClass('highlight');
+	});
+	$('#fact_3').click(function() {
+		$('#fact_3').toggleClass('highlight');
+	});
+	$('#fact_4').click(function() {
+		$('#fact_4').toggleClass('highlight');
+	});
+	$('#fact_5').click(function() {
+		$('#fact_5').toggleClass('highlight');
+	});
+	$('#fact_6').click(function() {
+		$('#fact_6').toggleClass('highlight');
+	});
+	$('#fact_7').click(function() {
+		$('#fact_7').toggleClass('highlight');
+	});
+	$('#fact_8').click(function() {
+		$('#fact_8').toggleClass('highlight');
+	});
+	$('#fact_9').click(function() {
+		$('#fact_9').toggleClass('highlight');
+	});
+	$('#fact_10').click(function() {
+		$('#fact_10').toggleClass('highlight');
+	});
+});

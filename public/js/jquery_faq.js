23d72554a756d85a7dad2a2bba7c7914c5325a6d
David R. Jex <david.jex@me.com>
2017-06-29 15:11:58 -0500

    Traversing exercises.

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -1,35 +1,19 @@
 $('document').ready(function() {
-	$('a').click(function() {
+	$('#make_details_invisible').click(function() {
 		$('dd').toggleClass('invisible');
 	});
-	$('#fact_1').click(function() {
-		$('#fact_1').toggleClass('highlight');
+	$('dt').click(function() {
+		$(this).toggleClass('highlight');
 	});
-	$('#fact_2').click(function() {
-		$('#fact_2').toggleClass('highlight');
+	$('#highlight_last_fact').click(function() {
+		$('ul').each(function(index, element) {
+			$(element).children().last().css('background', 'yellow')
+		});
 	});
-	$('#fact_3').click(function() {
-		$('#fact_3').toggleClass('highlight');
+	$('h3').click(function() {
+		$(this).next().css('font-weight', 'bold');
 	});
-	$('#fact_4').click(function() {
-		$('#fact_4').toggleClass('highlight');
-	});
-	$('#fact_5').click(function() {
-		$('#fact_5').toggleClass('highlight');
-	});
-	$('#fact_6').click(function() {
-		$('#fact_6').toggleClass('highlight');
-	});
-	$('#fact_7').click(function() {
-		$('#fact_7').toggleClass('highlight');
-	});
-	$('#fact_8').click(function() {
-		$('#fact_8').toggleClass('highlight');
-	});
-	$('#fact_9').click(function() {
-		$('#fact_9').toggleClass('highlight');
-	});
-	$('#fact_10').click(function() {
-		$('#fact_10').toggleClass('highlight');
+	$('li').click(function() {
+		$(this).parent('ul').children().first().css('color', 'blue');
 	});
 });
